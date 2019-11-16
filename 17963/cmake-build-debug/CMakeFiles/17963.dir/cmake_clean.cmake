aca6cdd5664cc24e2a690aed61d199e533d2829d
longzhitao <longzhitao.lynn@gmail.com>
2019-11-16 18:28:59 +0800

    17963 完美数

---
+++ 17963/cmake-build-debug/CMakeFiles/17963.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "17963.exe"
+  "17963.exe.manifest"
+  "17963.pdb"
+  "CMakeFiles/17963.dir/main.cpp.obj"
+  "lib17963.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17963.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
